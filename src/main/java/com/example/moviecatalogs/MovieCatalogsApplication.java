b0d73fdfd69a72c4502ed767d44ca96f62a6e072
teerox44 <anthony@publiclyme.com>
2024-09-09 14:31:22 -0300

    created movie catalogue service

---
+++ src/main/java/com/example/moviecatalogs/MovieCatalogsApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviecatalogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogsApplication.class, args);
+	}
+
+}
